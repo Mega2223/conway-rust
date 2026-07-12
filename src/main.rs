2efc8d7f7f94e2474e4c826780720c913243f825
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-07-11 23:15:34 -0300

    agr ss

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
-use std::{thread, time};
+use std::{process::exit, thread, time};
 use rand::Rng;
 
 const X: usize = 32;
 const Y: usize = 32;
 
+fn mapchar(v: i32) -> char {
+    return match v {
+        1 => '█',
+        0 => ' ',
+        _ => '?'
+    }
+}
+
 fn printmap(map: [[i32; X]; Y]){
+    print!("+ ");
     for x in map {
         for y in x {
-            print!("{y} ");
+            let c = mapchar(y);
+            print!("{c}{c}");
+        }
+        println!();
+    }
+}
+
+fn heatmap(map: [[i32; X]; Y]){
+    for x in 0..X {
+        for y in 0..Y {
+            let v = adjacencies(x, y, map);
+            print!("{v} ");
         }
         println!();
     }
@@ -18,35 +38,49 @@ fn adjacencies(x: usize, y: usize, map: [[i32; X]; Y]) -> i32 {
     let x_more: usize = (x + 1) % X;
     let y_less: usize = if y == 0 { Y - 1 } else { y - 1 };
     let y_more: usize = (y + 1) % Y;
-    
-    map[y][x_less] + map[y][x_more] + map[y_less][x] + map[y_more][x] +
-    map[y_less][x_less] + map [y_less][x_more] + map[y_more][x_less] + map[y_more][x_more]
+    map[x][y_less] + map[x][y_more] + map[x_less][y] + map[x_more][y]
+    + map[x_less][y_less] + map [x_less][y_more] + map[x_more][y_less] + map[x_more][y_more]
 }
 
 fn nextmap(current: [[i32; X];  Y], next: &mut [[i32; X]; Y]){
-    for i in 0..X {
-        for j in 0..Y {
-            let adjacencies = adjacencies(i, j, current);
-            if adjacencies == 2 || adjacencies == 3 {
-                next[i][j] = 1;
+    for x in 0..X {
+        for y in 0..Y {
+            let adjacencies = adjacencies(x, y, current);
+            let c_cell = current[x][y];
+            next[x][y] = if c_cell == 0 && adjacencies >= 3 {
+                1
+            } else if c_cell == 0 {
+                0
+            } else if adjacencies == 2 || adjacencies == 3 {
+                1
             } else {
-                next[i][j] = 0;
-            }
-            ;;let v = next[i][j];
-            // println!("[{i}][{j}] = {v}");
+                0
+            };
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("Olá mundo :)");
     let mut bmp: [[[i32; X]; Y]; 2] = [[[0; X]; Y]; 2];
+    
+    let n = 16;
 
     for x in 0..X {
-        for y in 0..Y {
-            bmp[0][x][y] = rand::thread_rng().gen_range(0..2);
+            for y in 0..Y {
+            bmp[0][x][y] = rand::thread_rng().gen_range(0..n) / (n-1);
         }
     }
+
+    /*
+    bmp[0][5][5] = 1;
+    bmp[0][5][6] = 1;
+    bmp[0][6][5] = 1;
+    bmp[0][6][6] = 1;
+    bmp[0][6][7] = 1;
+    */
+
+    let hmp = false;
 
     let mut i: usize = 0;
     loop {
@@ -54,7 +88,9 @@ fn main() {
         nextmap(bmp[i],&mut bmp[next]);
         println!("{i} -> {next}");
         printmap(bmp[i]);
+        //printmap(bmp[next]);
+        if hmp {heatmap(bmp[i]);}
         i = next;
-        thread::sleep(time::Duration::from_millis(100));
+        thread::sleep(time::Duration::from_millis(250));
     }
 }
