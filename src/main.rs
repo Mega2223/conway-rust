c84692fe2949a154d523abaf7595c0b9bc317599
Mega2223 <mega2223gamer@gmail.com>
2026-07-14 10:53:06 -0300

    Vetores dinamicos :o

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use std::{env,thread, time};
+use std::{env, thread, time, vec};
 use rand::Rng;
-
-const X: usize = 32;
-const Y: usize = 32;
 
 fn mapchar(v: i32) -> char {
     return match v {
@@ -12,8 +9,10 @@ fn mapchar(v: i32) -> char {
     }
 }
 
-fn printmap(map: [[i32; X]; Y]){
+fn printmap(map: & Vec<Vec<i32>>){
     print!("\n    ");
+    let X = map.len();
+    let Y = map[0].len();
     for _i in 0..Y-1 {
         let __i = _i % 10;
         print!("{__i} ");
@@ -47,7 +46,9 @@ fn printmap(map: [[i32; X]; Y]){
     println!("+");
 }
 
-fn heatmap(map: [[i32; X]; Y]){
+fn heatmap(map: & Vec<Vec<i32>>){
+    let X = map.len();
+    let Y = map[0].len();
     for x in 0..X {
         for y in 0..Y {
             let v = adjacencies(x, y, map);
@@ -57,7 +58,10 @@ fn heatmap(map: [[i32; X]; Y]){
     }
 }
 
-fn adjacencies(x: usize, y: usize, map: [[i32; X]; Y]) -> i32 {
+fn adjacencies(x: usize, y: usize, map: & Vec<Vec<i32>>) -> i32 {
+    let X = map.len();
+    let Y = map[0].len();
+
     let x_less: usize = if x == 0 { X - 1 } else { x - 1 };
     let x_more: usize = (x + 1) % X;
     let y_less: usize = if y == 0 { Y - 1 } else { y - 1 };
@@ -66,9 +70,12 @@ fn adjacencies(x: usize, y: usize, map: [[i32; X]; Y]) -> i32 {
     + map[x_less][y_less] + map [x_less][y_more] + map[x_more][y_less] + map[x_more][y_more]
 }
 
-fn nextmap(current: [[i32; X];  Y], next: &mut [[i32; X]; Y]){
+fn nextmap(current: & Vec<Vec<i32>>, next: &mut Vec<Vec<i32>>){
+    let X = current.len();
+    let Y = current[0].len();
 
-    for x in 0..X {
+    for x in 0..current.len() {
+        let row = current.get(x).expect("wuh?");
         for y in 0..Y {
             let adjacencies: i32 = adjacencies(x, y, current);
             let c_cell: i32 = current[x][y];
@@ -89,38 +96,56 @@ fn nextmap(current: [[i32; X];  Y], next: &mut [[i32; X]; Y]){
     }
 }
 
+fn resize(bmp: &mut Vec<Vec<i32>>, x: usize, y: usize){
+    let lx = bmp.len();
+    for _xi in lx..x {
+        bmp.push(vec!());
+    }
+    for xi in 0..x{
+        let row = bmp.get_mut(xi).expect("Error getting row {xi}");
+        let ly = row.len();
+        for _yi in ly..y {
+            row.push(0);
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     println!("Olá mundo :)");
-    let mut bmp: [[[i32; X]; Y]; 2] = [[[0; X]; Y]; 2];
-    
+    let mut bmp = [&mut vec!(),&mut vec!()];
     let n = 16;
+    let mut size_x = 32;
+    let mut size_y = 32;
 
-    for x in 0..X {
-            for y in 0..Y {
-            bmp[0][x][y] = rand::thread_rng().gen_range(0..n) / (n-1);
+    resize(bmp.get_mut(0).unwrap(), size_x, size_x);
+    resize(bmp.get_mut(1).unwrap(), size_y, size_y);
+    
+    let a = bmp.get_mut(0).unwrap();
+    for x in 0..size_x {
+            for y in 0..size_y {
+            a[x][y] = rand::thread_rng().gen_range(0..n) / (n-1);
         }
     }
-
-    /*
-    bmp[0][5][5] = 1;
-    bmp[0][5][6] = 1;
-    bmp[0][6][5] = 1;
-    bmp[0][6][6] = 1;
-    bmp[0][6][7] = 1;
-    */
 
     let hmp: bool = args.contains(&String::from("--heatmap"));
 
     let mut i: usize = 0;
     loop {
         let next = (i + 1) % 2;
-        nextmap(bmp[i],&mut bmp[next]);
+        let h = bmp.split_at_mut(1);
+        let t = if i == 0 {(h.0,h.1)} else {(h.1,h.0)};
+        
+        //let ue = t.0.get_mut(0).expect("f");
+        nextmap(
+            t.0.get_mut(0).expect("rip t0"),
+            t.1.get_mut(0).expect("rip t1")
+        );
         println!("{i} -> {next}");
         
-        if hmp {heatmap(bmp[i]);}
-        printmap(bmp[i]);
+        if hmp {heatmap(& bmp[i]);}
+        printmap(& bmp[i]);
         i = next;
         thread::sleep(time::Duration::from_millis(125));
     }
